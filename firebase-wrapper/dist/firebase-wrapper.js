720f344a4c43257885d7e22f23a33c232ccb7708
Peter Miller <pmiller@biscit.com.au>
2024-11-16 23:07:17 +1030

    install firebase npm package

---
+++ firebase-wrapper/dist/firebase-wrapper.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyD1j1JxchDa4kCjIVxKPpZTFZDc9a084fw",
+    authDomain: "496b-120-20-35-80.ngrok-free.app",
+    databaseURL: "https://half-now-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "half-now",
+    storageBucket: "half-now.appspot.com",
+    messagingSenderId: "1022216288188",
+    appId: "1:1022216288188:web:fbf335e1f0918eb1345db9",
+    measurementId: "G-04C5HYLHKH"
+};
+const firebase = initializeApp(firebaseConfig);
