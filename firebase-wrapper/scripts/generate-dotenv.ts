8f508206b583ec1fb79cdd39806b842679d4bea6
Peter Miller <pmiller@biscit.com.au>
2024-12-14 20:48:07 +1100

    introduce more types into dotenv

--- firebase-wrapper/scripts/generate-dotenv.ts
+++ firebase-wrapper/scripts/generate-dotenv.ts
@@ -2,14 +2,23 @@ import * as fs from "fs";
 import * as dotenv from "dotenv";
 import * as path from "path";
 
-type AllowedDotEnvTypes = string | number;
-
 enum dotenvFiles {
     base = ".env",
     dev = ".env.development",
 }
 
-const typeFieldPostfix: string = "_TYPE";
+const allowedDotEnvTypes = [String, Number, Boolean] as const;
+const defaultDotEnvTypeUpper: string = "STRING";
+
+const dotEnvTypeNames: string[] = allowedDotEnvTypes.map((type_) => type_.name);
+const regexKeyEmbeddedType = getRegexFromDotEnvTypes(dotEnvTypeNames);
+
+// something like: string | number
+type AllowedDotEnvTypes = (typeof allowedDotEnvTypes)[number] extends new (
+    ...args: any[]
+) => infer ReturnType
+    ? ReturnType
+    : never;
 
 const mustBePopulated: boolean = true;
 const baseDotenvConfig: dotenv.DotenvParseOutput | undefined = loadDotEnvFile(
@@ -33,7 +42,7 @@ const typedDotEnvConfig: Record<string, AllowedDotEnvTypes> =
     getTypedDotEnv(mergedDotEnvConfig);
 
 const dotenvTsContent = `export interface ProcessEnv {
-${dotenv2Types(typedDotEnvConfig)}
+${dotenv2TsTypes(typedDotEnvConfig)}
 }
 export const env: ProcessEnv = ${JSON.stringify(typedDotEnvConfig, null, 4)};
 `;
@@ -42,11 +51,22 @@ const outputFile: string = "./src/dotenv.ts";
 fs.writeFileSync(outputFile, dotenvTsContent, "utf8");
 console.log(`${outputFile} has been generated.`);
 if (process.env.NODE_ENV === "development") {
-    console.log(
-        `!!!!!
+    console.log(`
+!!!!!
 !!!!! BE CAREFUL NOT TO COMMIT THE DEVELOPMENT VALUES TO GIT!
-!!!!!`,
-    );
+!!!!!`);
+}
+
+// functions
+
+/** the types of the variable are stored on the end of the key after __
+ * eg. MY_VAR__NUMBER */
+function getRegexFromDotEnvTypes(allowedDotEnvTypes: string[]): RegExp {
+    const typesAsString: string = allowedDotEnvTypes
+        .map((typeName: string) => typeName.toUpperCase())
+        .join("|");
+
+    return new RegExp(`_(${typesAsString})$`);
 }
 
 function loadDotEnvFile(
@@ -94,38 +114,56 @@ function mergeDotEnvConfigs(
     )!;
 }
 
-function dotenv2Types(
-    parsedDotenvVars: Record<string, AllowedDotEnvTypes>,
-): string {
-    const tab: string = "    ";
-    return Object.keys(parsedDotenvVars)
-        .filter((key: string) => !key.endsWith(typeFieldPostfix))
-        .map((key: string) => `${tab}${key}: ${typeof parsedDotenvVars[key]};`)
-        .join("\n");
-}
-
 function getTypedDotEnv(
     parsedDotenvVars: NodeJS.ProcessEnv,
 ): Record<string, AllowedDotEnvTypes> {
-    return Object.keys(parsedDotenvVars)
-        .filter((key: string) => !key.endsWith(typeFieldPostfix))
-        .reduce<Record<string, AllowedDotEnvTypes>>((acc, key) => {
-            const typeAsString: string = getType(parsedDotenvVars, key);
-            switch (typeAsString) {
-                case "number":
-                    acc[key] = Number(parsedDotenvVars[key]);
-                    break;
-                default:
-                    acc[key] = parsedDotenvVars[key]!;
-                    break;
-            }
-            return acc;
-        }, {});
+    return Object.keys(parsedDotenvVars).reduce<
+        Record<string, AllowedDotEnvTypes>
+    >((acc, key) => {
+        const { updatedKey, keyTypeNameUpper } = extractTypeFromKey(key);
+        switch (keyTypeNameUpper) {
+            case "BOOLEAN":
+                acc[updatedKey] = parsedDotenvVars[key] === "true";
+                break;
+            case "NUMBER":
+                acc[updatedKey] = Number(parsedDotenvVars[key]);
+                break;
+            case "STRING":
+                acc[updatedKey] = parsedDotenvVars[key]!;
+                break;
+            default:
+                throw new Error(
+                    `Type ${keyTypeNameUpper} is not supported ` +
+                        `(from .env var ${key})`,
+                );
+        }
+        return acc;
+    }, {});
 }
 
-function getType(parsedDotenvVars: NodeJS.ProcessEnv, key: string): string {
-    if (parsedDotenvVars.hasOwnProperty(`${key}${typeFieldPostfix}`)) {
-        return parsedDotenvVars[`${key}${typeFieldPostfix}`]!;
-    }
-    return "string";
+function extractTypeFromKey(key: string): {
+    updatedKey: string;
+    keyTypeNameUpper: string;
+} {
+    const capturedGroups: RegExpExecArray | null =
+        regexKeyEmbeddedType.exec(key);
+
+    const keyTypeNameUpper: string =
+        capturedGroups === null ? defaultDotEnvTypeUpper : capturedGroups[1];
+
+    const updatedKey = key.replace(regexKeyEmbeddedType, "");
+
+    return { updatedKey, keyTypeNameUpper };
+}
+
+function dotenv2TsTypes(
+    parsedDotenvVars: Record<string, AllowedDotEnvTypes>,
+): string {
+    const tab: string = "    ";
+
+    const processEnvTypes = Object.keys(parsedDotenvVars)
+        .map((key: string) => `${tab}${key}: ${typeof parsedDotenvVars[key]};`)
+        .join("\n");
+
+    return processEnvTypes;
 }
--- firebase-wrapper/src/dotenv.ts
+++ firebase-wrapper/src/dotenv.ts
@@ -9,6 +9,7 @@ export interface ProcessEnv {
     FIREBASE_MESSAGING_SENDER_ID: string;
     FIREBASE_APP_ID: string;
     FIREBASE_MEASUREMENT_ID: string;
+    FIREBASE_LINK_ACCOUNTS: boolean;
     PROJECT_DOMAIN: string;
     PROJECT_NAME: string;
 }
@@ -23,6 +24,7 @@ export const env: ProcessEnv = {
     "FIREBASE_MESSAGING_SENDER_ID": "190381967079",
     "FIREBASE_APP_ID": "1:190381967079:web:9436046b42e133ebf18442",
     "FIREBASE_MEASUREMENT_ID": "G-QZB6RJE8ZS",
+    "FIREBASE_LINK_ACCOUNTS": true,
     "PROJECT_DOMAIN": "auth.null.place",
     "PROJECT_NAME": "Auth Test"
 };
